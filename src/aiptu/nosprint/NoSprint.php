93354571d03d5f6e07c27b63aa1a68c0e657ac3c
AIPTU <anangp808@gmail.com>
2021-10-20 14:50:14 +0700

    CS

--- src/aiptu/nosprint/NoSprint.php
+++ src/aiptu/nosprint/NoSprint.php
@@ -8,45 +8,47 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerToggleSprintEvent;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
+use function gettype;
+use function in_array;
 
 class NoSprint extends PluginBase implements Listener
 {
-    public function onEnable(): void
-    {
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->checkConfig();
-    }
+	public function onEnable(): void
+	{
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->checkConfig();
+	}
 
-    private function checkConfig(): void
-    {
-        $this->saveDefaultConfig();
-        foreach ([
-            "message" => "string",
-            "blacklisted-worlds" => "array"
-        ] as $option => $expectedType) {
-            if (($type = gettype($this->getConfig()->getNested($option))) !== $expectedType) {
-                throw new \TypeError("Config error: Option ($option) must be of type $expectedType, $type was given");
-            }
-        }
-    }
+	private function checkConfig(): void
+	{
+		$this->saveDefaultConfig();
+		foreach ([
+			"message" => "string",
+			"blacklisted-worlds" => "array"
+		] as $option => $expectedType) {
+			if (($type = gettype($this->getConfig()->getNested($option))) !== $expectedType) {
+				throw new \TypeError("Config error: Option ($option) must be of type $expectedType, $type was given");
+			}
+		}
+	}
 
-    public function onPlayerToggleSprint(PlayerToggleSprintEvent $event): void
-    {
-        $player = $event->getPlayer();
+	public function onPlayerToggleSprint(PlayerToggleSprintEvent $event): void
+	{
+		$player = $event->getPlayer();
 
-        if ($event->isCancelled()) {
-            return;
-        }
-        if ($player->hasPermission("nosprint.bypass")) {
-            return;
-        }
-        if (!in_array($player->getLevelNonNull()->getFolderName(), $this->getConfig()->getAll()["blacklisted-worlds"])) {
-            return;
-        }
-        if ($player->isSprinting()) {
-            $player->setSprinting(!$player->isSprinting());
-        }
-        $event->setCancelled();
-        $player->sendMessage(TextFormat::colorize($this->getConfig()->get("message", "&cYou can't sprint in this world")));
-    }
+		if ($event->isCancelled()) {
+			return;
+		}
+		if ($player->hasPermission("nosprint.bypass")) {
+			return;
+		}
+		if (!in_array($player->getLevelNonNull()->getFolderName(), $this->getConfig()->getAll()["blacklisted-worlds"], true)) {
+			return;
+		}
+		if ($player->isSprinting()) {
+			$player->setSprinting(!$player->isSprinting());
+		}
+		$event->setCancelled();
+		$player->sendMessage(TextFormat::colorize($this->getConfig()->get("message", "&cYou can't sprint in this world")));
+	}
 }
