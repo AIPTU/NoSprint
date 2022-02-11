694067610a74f6e25c1eb48a7404225bc0edb50f
AIPTU <anangp808@gmail.com>
2022-02-11 15:42:08 +0700

    Code improvements

--- src/NoSprint.php
+++ src/NoSprint.php
@@ -1,91 +1,89 @@
 <?php
 
+/*
+ *
+ * Copyright (c) 2021 AIPTU
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
 declare(strict_types=1);
 
 namespace aiptu\nosprint;
 
-use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerToggleSprintEvent;
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\TextFormat;
 use pocketmine\world\World;
-use function gettype;
 use function in_array;
 use function rename;
 
-final class NoSprint extends PluginBase implements Listener
+final class NoSprint extends PluginBase
 {
+	private const CONFIG_VERSION = 1.0;
+
 	private const MODE_BLACKLIST = 0;
 	private const MODE_WHITELIST = 1;
 
 	private int $mode;
 
+	private TypedConfig $typedConfig;
+
 	public function onEnable(): void
 	{
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-
 		$this->checkConfig();
+
+		$this->getServer()->getPluginManager()->registerEvents(new EventHandler($this), $this);
 	}
 
-	public function onPlayerToggleSprint(PlayerToggleSprintEvent $event): void
+	public function getTypedConfig(): TypedConfig
 	{
-		if ($event->isCancelled()) {
-			return;
+		return $this->typedConfig;
+	}
+
+	public function checkWorld(World $world): bool
+	{
+		if ($this->mode === self::MODE_BLACKLIST) {
+			return !(in_array($world->getFolderName(), $this->getTypedConfig()->getStringList('worlds.list'), true));
 		}
 
-		$player = $event->getPlayer();
-
-		if (!$this->checkWorlds($player->getWorld())) {
-			return;
-		}
-
-		if ($player->hasPermission('nosprint.bypass')) {
-			return;
-		}
-
-		if ($player->isSprinting()) {
-			$player->setSprinting(false);
-		}
-
-		$player->sendPopup(TextFormat::colorize($this->getConfig()->get('message', "&cYou can't sprint in this world")));
-		$event->cancel();
+		return in_array($world->getFolderName(), $this->getTypedConfig()->getStringList('worlds.list'), true);
 	}
 
 	private function checkConfig(): void
 	{
 		$this->saveDefaultConfig();
 
-		if ($this->getConfig()->get('config-version', 1) !== 1) {
-			$this->getLogger()->notice('Your configuration file is outdated, updating the config.yml...');
-			$this->getLogger()->notice('The old configuration file can be found at config.old.yml');
-
-			rename($this->getDataFolder() . 'config.yml', $this->getDataFolder() . 'config.old.yml');
-
+		if (!$this->getConfig()->exists('config-version') || ($this->getConfig()->get('config-version', self::CONFIG_VERSION) !== self::CONFIG_VERSION)) {
+			$this->getLogger()->warning('An outdated config was provided attempting to generate a new one...');
+			if (!rename($this->getDataFolder() . 'config.yml', $this->getDataFolder() . 'config.old.yml')) {
+				$this->getLogger()->critical('An unknown error occurred while attempting to generate the new config');
+				$this->getServer()->getPluginManager()->disablePlugin($this);
+			}
 			$this->reloadConfig();
 		}
 
-		foreach ([
-			'message' => 'string',
-			'worlds.list' => 'array',
-		] as $option => $expectedType) {
-			if (($type = gettype($this->getConfig()->getNested($option))) !== $expectedType) {
-				throw new \TypeError("Config error: Option ({$option}) must be of type {$expectedType}, {$type} was given");
-			}
-		}
+		$this->typedConfig = new TypedConfig($this->getConfig());
 
-		match ($this->getConfig()->getNested('worlds.mode')) {
+		match ($this->getTypedConfig()->getString('worlds.mode', 'blacklist')) {
 			'blacklist' => $this->mode = self::MODE_BLACKLIST,
 			'whitelist' => $this->mode = self::MODE_WHITELIST,
 			default => throw new \InvalidArgumentException('Invalid mode selected, must be either "blacklist" or "whitelist"!'),
 		};
-	}
-
-	private function checkWorlds(World $world): bool
-	{
-		if ($this->mode === self::MODE_BLACKLIST) {
-			return !(in_array($world->getFolderName(), $this->getConfig()->getAll()['worlds']['list'], true));
-		}
-
-		return in_array($world->getFolderName(), $this->getConfig()->getAll()['worlds']['list'], true);
 	}
 }
